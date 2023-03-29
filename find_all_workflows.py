ee9ad001ce1e6c27a09a6073d1cf3275b467dc43
aftab <aftab.saeedi2015@gmail.com>
2023-03-29 11:16:24 +0530

    every link now has id class and xpath and some unnecessary code removed

--- find_all_workflows.py
+++ find_all_workflows.py
@@ -29,24 +29,17 @@ b_url = 'https://www.demoblaze.com'
 def generate_unique_id():
     return str(uuid.uuid4())
 
-def crawl(url, visited_links, current_workflow,parent_id,tag):
+def crawl(url, visited_links, current_workflow,parent_id,tag,e_id,e_class,e_xpath):
     parent = {
         'id': generate_unique_id(),
         'url': url,
-        'locators':[],
+        'locators':[{'id':e_id,'class':e_class,'xpath':e_xpath}],
         'tag':tag,
         'parent_id':parent_id,
         'children': []
     }
     # print(parent)
     if url in visited_links:
-        # f = open("demo3.json", "a")
-        # f.write(json.dumps(parent))
-        # f.write("\n")
-        # f.close()
-
-        # get screenshots
-
         # Remove the current link from the current workflow and return
         if(len(current_workflow)!=0):
             k = current_workflow.pop()
@@ -55,46 +48,31 @@ def crawl(url, visited_links, current_workflow,parent_id,tag):
     # Add the URL to the set of visited links
     else:
         visited_links.add(url)
-
-    # Add the default protocol if none is supplied
-
-
     # Send an HTTP request to the URL and retrieve the response
     # print(url)
     response = requests.get(url)
 
     # Parse the HTML content of the response using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
-
-    # # Find all links on the page
-    links = soup.find_all('a',href=True)
     # driver.get(b_url)
     # links = driver.find_elements(By.TAG_NAME, 'a')
 
     style_tag = soup.find('style')
     # Remove the style tag if it exists
-
     if style_tag is not None:
         style_tag.extract()
-     # Create a new object for the parent
+     # Create a etree object  from the soup
     lxml_tree = etree.fromstring(str(soup))
     a_tags = lxml_tree.findall('.//a')
-    for a_tag in a_tags:
-        print(a_tag.attrib['class'])
-    # If there are no new links
-    if not links:
-        # f = open("demo3.json", "a")
-        # f.write(json.dumps(parent))
-        # f.write("\n")
-        # f.close()
+    if not a_tags:
         if(len(current_workflow)!=0):
             k = current_workflow.pop()
         return parent
 
     # Loop through new links and add them to the current workflow and visited links
     else:
-        for link in links:
-            new_url = link['href']
+        for link in a_tags:
+            new_url = link.attrib['href']
             new_tag = assign_tag(new_url)
             if not new_url.startswith('http'):
                 if not new_url.startswith('/'):
@@ -102,16 +80,20 @@ def crawl(url, visited_links, current_workflow,parent_id,tag):
                 new_url = b_url+new_url
             # check if internal link
             if new_url is not None and new_url.startswith(b_url):
-                # e_id = link.get_attribute('id')
-                # e_class = link.get_attribute('class')
-                print('linkk',link)
-                lxml_element = lxml_tree.xpath('//%s[@*="%s"]' % (link.name, link.attrs))
-                root_element = lxml_tree.getroottree().getroot()
-                xpath = root_element.getpath(lxml_element[0])
-                print('xpath ',xpath)
-                html = driver.page_source
-                current_workflow.append(new_url)
-                child = crawl(new_url, visited_links, current_workflow,parent['id'],new_tag)
+                print(link.attrib)
+                # check if attribute exists
+                e_id=''
+                e_class=''
+                if 'id' in link.attrib:
+                    e_id = link.attrib['id']
+                    print(e_id)
+                if 'class' in link.attrib:
+                    e_class = link.attrib['class']
+                    print(e_class)
+                tree = lxml_tree.getroottree()
+                xpath = tree.getpath(link)
+                print("xpath",xpath)
+                child = crawl(new_url, visited_links, current_workflow,parent['id'],new_tag,e_id,e_class,xpath)
                 parent['children'].append(child)
 
     # remove the parent after all its children are looped through
@@ -125,7 +107,7 @@ def crawl(url, visited_links, current_workflow,parent_id,tag):
 visited_links = set()
 current_workflow = ['https://www.demoblaze.com']
 
-parent1 = crawl(b_url, visited_links, current_workflow,0,'home')
+parent1 = crawl(b_url, visited_links, current_workflow,0,'home','id','class','xpath')
 f = open("demo4.json", "a")
 f.write(json.dumps(parent1))
 f.write("\n")
